4abc6e87ded69044407b1713eb6a84247aa18ef4
evan miller <emille4@clemson.edu>
2017-01-25 16:55:06 -0500

    attempted question 8

--- refactor.js
+++ refactor.js
@@ -227,7 +227,17 @@ var decreaseScore = function() {
 };
 
 // Put your answer below -------------------------
+function protection (){
+  var score = 0;
 
+  var increaseScore = function() {
+    score++;
+  };
+
+  var decreaseScore = function() {
+    score--;
+  };
+}
 
 // -----------------------------------------------
 
